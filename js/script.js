4d9c6e188bd1df8b20b346a0442bd6b0fd6ae524
anbarasanrc <125238722+anbarasanrc@users.noreply.github.com>
2025-07-13 13:20:07 +0530

    contact fix respond

--- js/script.js
+++ js/script.js
@@ -86,7 +86,7 @@ function former() {
      var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
-        let rsuc, rtext = JSON.parse(this.responseText)
+        let { rsuc, rtext } = JSON.parse(this.responseText)
         document.getElementById("contactresponce").innerHTML = rtext;
       }
     };
